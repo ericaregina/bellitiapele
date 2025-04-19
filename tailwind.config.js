86a24870a7bacdd3b8e14e2eb8fc54a11232c3a4
ericaregina <ribeiroericacomc@gmail.com>
2025-04-18 22:26:05 -0300

    melhorias no front

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-beige': '#c8b08c',  // Nome da cor personalizada
+        },
+      },
+    },
+    plugins: [],
+  }
+  
